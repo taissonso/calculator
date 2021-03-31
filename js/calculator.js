8813c91d345855940e51ff957fde5494f4bf1a85
Taisson Silveira de Oliveira <taisson.oliveira@gmail.com>
2021-03-30 19:12:02 -0700

    Update JS
    Ajustado funções de atualizarVisor e negativeNumber para aceitar a formatação correta de números inteiros e decimas.

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,14 @@
-
 onload = () => {
+    
+    /* Pega o valor do localStorage, caso exita algum valor já ativa o modo dark/light*/
     const modeLight = localStorage.getItem('modeLightActive');
-
-    if(modeLight) {
+    if (modeLight) {
         document.querySelector('body').setAttribute("light", "true");
         document.querySelector(".btn-mode").innerHTML = "Dark Mode";
         document.querySelector('#btn-mode-light').checked = true;
     }
 
+    /* --- Seleciona o checkbox para ativar/desativar o modo dark */
     document.querySelector('#btn-mode-light').onclick = modeDark;
 
     /* --- Seleciona o botão clicado e envia para a função cliqueBotaoDigito */
@@ -39,8 +40,8 @@ let novoValor = true;
  * Muda para o modo Light ou modo Dark se clicar no chebox ou label.
  * Em caso de selecionar, atualiza o localStorage para saber quando o usuario voltar.
  */
-const modeDark = ()=> {
-    if(document.querySelector('#btn-mode-light').checked) {
+const modeDark = () => {
+    if (document.querySelector('#btn-mode-light').checked) {
         document.querySelector('body').setAttribute("light", "true");
         document.querySelector(".btn-mode").innerText = "Dark Mode";
         localStorage.setItem('modeLightActive', true);
@@ -53,22 +54,55 @@ const modeDark = ()=> {
 
 /**
  * Ao clicar em botão atualiza o visor de insersão de digitos.
+ * Separa o valor que irá para o visor em inteiros e decimas. 
+ * Se o valor estiver com '-', remove ele e depois passa por um for para colocar a ',' nos números inteiros a cada 3 casas 
+ * para separar por mil, milhares, milhões e etc.
+ * Faz a verificação para inteiros começando com 0 para tratar exemplos como 0.154
+ * Dentro do else faz outra verificação para aceitar somente um número inteiros + '.'
+ * e Por fim atualiza o número com inteiros e decimais, com a formatação de ',' e '.'
  */
-const atualizaVisor = ()=> {
-    document.querySelector('#inserido').innerText = valorClicado;
-} 
+const atualizarVisor = () => {
+    let partesNumero = valorClicado.split('.');
+    let inteiros = partesNumero[0];
+    let decimais = partesNumero[1];
+    let valorAtualizado = '';
+    let aux = 0;
+    let flag = false;
+   
+    inteiros.indexOf('-') != -1 ? ( inteiros = inteiros.slice(1), flag = true ): flag = false;
+   
+    for (let i = inteiros.length - 1; i >= 0; i--) {
+        if (++aux > 3) {
+            valorAtualizado = ',' + valorAtualizado;
+            aux = 1;
+        }
+        valorAtualizado = inteiros[i] + valorAtualizado;
+    }
+
+    if ( inteiros === '0' && decimais === '') {
+        valorAtualizado = '0.';   
+    } else {
+        if(inteiros > 0 && decimais === '') {
+            valorAtualizado = valorAtualizado + '.';
+        } else valorAtualizado = valorAtualizado + (decimais ? '.' + decimais : '');
+    }    
+    if (flag) {
+        document.querySelector('#inserido').innerText = '-' + valorAtualizado;
+    } else document.querySelector('#inserido').innerText = valorAtualizado;
+}
 
 /**
- * Ao clicar em algum botão de digito pega o valor e manda para a função de atualizar visor.
- * Verifica se o valor é novo para a primeira insersão para não deixar o zero do visor inicial junto.
+ * Ao clicar em algum botão de digito pega o valor e manda para a função atualizarVisor().
+ * Verifica se o valor é novo para a primeira insersão para não deixar o zero do visor inicial junto. Ex: 02
+ * E se o novo valor for 0 na primeira insersão simplesmente só atualiza a variável novoValor pra true para não fica inserindo
+ * zero consecutivos. Ex:000000 
  * E atualiza a variável booleana para false.
  */
 const cliqueBotaoDigito = (digito) => {
-    if(novoValor){
+    if (novoValor) {
         digito === '0' ? (novoValor = true) : (valorClicado = digito, novoValor = false);
-        console.log(valorClicado)
     } else valorClicado += digito;
-        atualizaVisor();
+    atualizarVisor();
 }
 
 /**
@@ -89,7 +123,7 @@ const clearAll = () => {
  */
 const clearLast = () => {
 
-    if(valorClicado === "-0." || valorClicado === "0.") {
+    if (valorClicado === "-0." || valorClicado === "0.") {
         valorClicado = '0';
         novoValor = true;
     }
@@ -103,23 +137,28 @@ const clearLast = () => {
         valorClicado = '0';
         novoValor = true;
     } else valorClicado = valorClicado.slice(0, valorClicado.length - 1);
-    atualizaVisor();
+    atualizarVisor();
 }
 
 /**
- * Ao clicar no botão de +/- irá negativa o número ou não.
+ * Ao clicar no botão de +/- irá negativar o número ou não.
+ * Verifica a existencia do '-' com indexOf, se retornar -1 verifica o valor já digitado.
+ *     - Se o valor for zero então ativa ev.preventDefault, caso contrário adiciona '-' a frente do valor. 
+ * Caso tenha a existencia '-' o mesmo é removido com o metodo slice do (1, ao final da string[número]) 
  */
 
 const negativeNumber = (ev) => {
-    if( valorClicado.indexOf('-') === -1) {
-        valorClicado === "0" ? ev.preventDefault() : valorClicado = "-"+ valorClicado; atualizaVisor();
+    if (valorClicado.indexOf('-') === -1) {
+        valorClicado === "0" ? ev.preventDefault() : valorClicado = "-" + valorClicado;
     } else valorClicado = valorClicado.slice(1, valorClicado.length);
-    atualizaVisor()
+    atualizarVisor();
 }
 
 /**
  * Coloca o ponto para número flutuante
+ * Verifica a existencia do '.' com indexOf, se retornar -1 então coloca o ponto no número e chama a função de atualizarVisor,
+ * caso já tenha o ponto simplesmente faz ev.prenventDefault()
  */
 const colocaPonto = (ev) => {
-   valorClicado.indexOf('.') === -1 ? ( valorClicado = valorClicado + ".", atualizaVisor(), novoValor = false ) : ev.preventDefault();
-}
+    valorClicado.indexOf('.') === -1 ? ( valorClicado = valorClicado + ".", atualizarVisor(), novoValor = false ) : ev.preventDefault();
+};
